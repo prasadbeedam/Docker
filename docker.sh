172d8308e96e0067fd660d5ec1dee6b61db232ad
prasad <prasad@gmail.com>
2024-08-11 07:54:41 +0530

    docker-files

---
+++ docker.sh
@@ -0,0 +1,7 @@
+# install as root user
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker ec2-user
+#sudo systemctl status docker
+sudo systemctl start docker
